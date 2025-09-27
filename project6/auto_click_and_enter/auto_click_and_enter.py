263fa2e17df216f8aa03ed87ae09a14153e4d51f
doaa292 <doaa.mansour@learner.42.tech>
2025-09-28 02:10:41 +0300

    Update auto_click_and_enter.py

--- project6/auto_click_and_enter/auto_click_and_enter.py
+++ project6/auto_click_and_enter/auto_click_and_enter.py
@@ -17,9 +17,9 @@ time.sleep(0.5)
 pyautogui.moveTo(1202, 157, duration=1)
 pyautogui.click()
 time.sleep(0.5)
-pyautogui.moveTo(705, 62, duration=1
-                 )
+pyautogui.moveTo(705, 62, duration=1)
 pyautogui.click()
 time.sleep(0.5)
+
 pyautogui.typewrite("How to use pyautogui lib?", interval=0.05)
 pyautogui.press('enter')
